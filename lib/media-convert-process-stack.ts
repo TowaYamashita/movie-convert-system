162826939c358c9b2b94567ed45c4bb9d3a2638e
TowaYamashita <Confirm.Apps.Develop@gmail.com>
2023-12-12 23:03:01 +0900

    リファクタ

---
+++ lib/media-convert-process-stack.ts
@@ -0,0 +1,84 @@
+import { Construct } from "constructs";
+import { Bucket, EventType } from 'aws-cdk-lib/aws-s3';
+import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
+import { LambdaDestination } from 'aws-cdk-lib/aws-s3-notifications';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+
+interface MediaConvertProcessProps {
+  /**
+   * MediaConvert のアカウント別エンドポイント
+   * @example https://xxxxxxxxx.mediaconvert.us-east-1.amazonaws.com
+   */
+  customerMediaConvertEndpoint: string;
+  /**
+   * MediaConvert のキューのARN
+   */
+  queueArn: string;
+  /**
+   * 変換元動画のアップロード先のS3バケット
+   */
+  inputBucket: Bucket;
+  /**
+   * MediaConvert でジョブを実行する際に渡すIAM Role の ARN
+   */
+  queueIamRoleArn: string;
+  /**
+   * MediaConvert のジョブを作成する際に使う解像度別出力プリセット
+   */
+  outputPresetArns: Record<'360p' | '720p' | '1080p', string>;
+  /**
+   * 変換元動画のプレフィックス
+   */
+  inputPrefix: string;
+  /**
+   * 変換後動画のプレフィックス
+   */
+  outputPrefix: string;
+  /**
+   * MediaConvert のジョブを作成する処理を実行する Lambda に割当てる IAM Role
+   */
+  createJobPolicy: PolicyStatement;
+}
+
+export class MediaConvertProcessStack extends Construct {
+  constructor(scope: Construct, id: string, props: MediaConvertProcessProps) {
+    super(scope, id);
+
+    const mediaConvertLambda = new Function(this, 'MediaConvertLambda', {
+      functionName: 'submit-job',
+      runtime: Runtime.NODEJS_20_X,
+      handler: 'index.handler',
+      code: Code.fromAsset('lambda/submit_job'),
+      environment: {
+        MEDIA_CONVERT_ENDPOINT: props.customerMediaConvertEndpoint,
+        QUEUE_ARN: props.queueArn,
+        MOVIE_CONVERT_BUCKET_ARN: props.inputBucket.s3UrlForObject(),
+        IAM_ROLE_ARN: props.queueIamRoleArn,
+        OUTPUT_PRESET_360P_ARN: props.outputPresetArns['360p'],
+        OUTPUT_PRESET_720P_ARN: props.outputPresetArns['720p'],
+        OUTPUT_PRESET_1080P_ARN: props.outputPresetArns['1080p'],
+        INPUT_PREFIX: props.inputPrefix,
+        OUTPUT_PREFIX: props.outputPrefix,
+      },
+    });
+    mediaConvertLambda.addToRolePolicy(props.createJobPolicy);
+    mediaConvertLambda.addToRolePolicy(new PolicyStatement({
+      actions: [
+        's3:*',
+      ],
+      resources: ['*'],
+    }));
+    mediaConvertLambda.addToRolePolicy(new PolicyStatement({
+      actions: [
+        'iam:PassRole',
+      ],
+      resources: [props.queueIamRoleArn],
+    }));
+
+    props.inputBucket.addEventNotification(
+      EventType.OBJECT_CREATED,
+      new LambdaDestination(mediaConvertLambda),
+      { prefix: `${props.inputPrefix}/` }
+    );
+  }
+}
--- lib/movie-convert-system-stack.ts
+++ lib/movie-convert-system-stack.ts
@@ -1,13 +1,11 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { EventType } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 import { MediaConvetStack } from './media-convert-stack';
 import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
-import { LambdaDestination } from 'aws-cdk-lib/aws-s3-notifications';
-import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { Rule } from 'aws-cdk-lib/aws-events';
 import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
 import { InputBucketStack } from './input-bucket-stack';
+import { MediaConvertProcessStack } from './media-convert-process-stack';
 
 interface StageContext {
   inputBucketName: string;
@@ -38,43 +36,17 @@ export class MovieConvertSystemStack extends Stack {
       outputPrefix: context.outputPrefix,
     });
 
-    // 動画変換作成ジョブを発行する Lambda関数
-    const mediaConvertLambda = new Function(this, 'MediaConvertLambda', {
-      functionName: 'submit-job',
-      runtime: Runtime.NODEJS_20_X,
-      handler: 'index.handler',
-      code: Code.fromAsset('lambda/submit_job'),
-      environment: {
-        MEDIA_CONVERT_ENDPOINT: context.customerMediaConvertEndpoint,
-        QUEUE_ARN: queue.arn,
-        MOVIE_CONVERT_BUCKET_ARN: inputBucket.bucket.s3UrlForObject(),
-        IAM_ROLE_ARN: queue.iamRoleArn,
-        OUTPUT_PRESET_360P_ARN: queue.outputPresetArns['360p'],
-        OUTPUT_PRESET_720P_ARN: queue.outputPresetArns['720p'],
-        OUTPUT_PRESET_1080P_ARN: queue.outputPresetArns['1080p'],
-        INPUT_PREFIX: context.inputPrefix,
-        OUTPUT_PREFIX: context.outputPrefix,
-      },
+    // 動画変換作成ジョブの発行&実行
+    new MediaConvertProcessStack(this, 'MediaConvertProcess', {
+      customerMediaConvertEndpoint: context.customerMediaConvertEndpoint,
+      queueArn: queue.arn,
+      inputBucket: inputBucket.bucket,
+      queueIamRoleArn: queue.iamRoleArn,
+      outputPresetArns: queue.outputPresetArns,
+      inputPrefix: context.inputPrefix,
+      outputPrefix: context.outputPrefix,
+      createJobPolicy: queue.createJobPolicy,
     });
-    mediaConvertLambda.addToRolePolicy(queue.createJobPolicy);
-    mediaConvertLambda.addToRolePolicy(new PolicyStatement({
-      actions: [
-        's3:*',
-      ],
-      resources: ['*'],
-    }));
-    mediaConvertLambda.addToRolePolicy(new PolicyStatement({
-      actions: [
-        'iam:PassRole',
-      ],
-      resources: [queue.iamRoleArn],
-    }));
-
-    inputBucket.bucket.addEventNotification(
-      EventType.OBJECT_CREATED,
-      new LambdaDestination(mediaConvertLambda),
-      { prefix: `${context.inputPrefix}/` }
-    );
 
     // 動画変換に成功した場合のフローを定義
     const eventSuccessRule = new Rule(this, 'MediaConvertSuccessRule', {
